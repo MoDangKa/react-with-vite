7bad573b59dc59b753166e48a471e09d789a5bc0
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2025-04-28 02:03:34 +0700

    feat: create layout components

---
+++ src/layouts/SecondaryLayout.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const SecondaryLayout: React.FC<Props> = () => {
+  return (
+    <div className="container mx-auto min-h-screen flex flex-col">
+      {/* Header */}
+      <header className="bg-sky-700 p-4 text-white">
+        <h1>Secondary Layout</h1>
+      </header>
+
+      {/* Main Content */}
+      <main className="grow p-4">
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-sky-700 p-2 text-white mt-4">
+        <p>&copy; 2025 Another Layout Footer</p>
+      </footer>
+    </div>
+  );
+};
+
+export default SecondaryLayout;
